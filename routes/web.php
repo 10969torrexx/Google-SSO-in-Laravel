d97d39fab80f23ee819a4ee964495a81fca15357
10969torrexx <83467428+10969torrexx@users.noreply.github.com>
2024-05-07 21:03:00 +0800

    Adding Laravel Auth and Google OAuth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/login/google', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('/login/google/callback', function () {
+    $user = Socialite::driver('google')->user();
+    // Handle the authenticated user
+    // For example, create a new user if it doesn't exist or log in the existing user
 });
